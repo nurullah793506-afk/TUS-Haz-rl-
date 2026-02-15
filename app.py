9c859da8a8933bda0255da9d910cea8ba266ebfb
nurullah793506-afk <nurullah793506@gmail.com>
2026-02-15 23:40:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pytz
 # ===================== AYARLAR =====================
 TIMEZONE = pytz.timezone("Europe/Istanbul")
 MORNING_TIME = time(8, 0)
-EVENING_TIME = time(20, 0)
+EVENING_TIME = time(23, 40)
 GUNLUK_SORU_SAYISI = 5
 
 QUESTIONS_FILE = "questions.json"
