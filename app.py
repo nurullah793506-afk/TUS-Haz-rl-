6b7dd7633b0afed9545245320612e68f3c09231d
nurullah793506-afk <nurullah793506@gmail.com>
2026-03-02 15:43:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pytz
 
 # ===================== AYARLAR =====================
 TIMEZONE = pytz.timezone("Europe/Istanbul")
-MORNING_TIME = time(08, 00)
+MORNING_TIME = time(16, 00)
 EVENING_TIME = time(20, 00)
 GUNLUK_SORU_SAYISI = 10
 
