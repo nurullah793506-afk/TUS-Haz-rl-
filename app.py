357ca3bc7a9584e0ac0d6e3a747d712a47466910
nurullah793506-afk <nurullah793506@gmail.com>
2026-03-08 00:05:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pytz
 # ===================== AYARLAR =====================
 TIMEZONE = pytz.timezone("Europe/Istanbul")
 MORNING_TIME = time(00, 4)
-EVENING_TIME = time(23, 59)
+EVENING_TIME = time(00, 5)
 GUNLUK_SORU_SAYISI = 10
 
 st.set_page_config(page_title="Günün Seçilmiş Soruları", page_icon="🌸")
